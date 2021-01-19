f37f5216316e4a5f281c72be11d2161d54f0cd3c
Kanatbek Beishenaliev <kanatbey@Kanatbeks-iMac.local>
2021-01-19 18:06:41 +0900

    added todo_list to test page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
 
 
 
@@ -6,19 +7,8 @@ def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
 
 def second(request):
     return HttpResponse("This is new test 2 page!")
-
-def third(request):
-    return HttpResponse("This is page test 3")
-
-def task31(request):
-    return HttpResponse("Ваша запись была добавлена успешно.")
-
-def task31_2(request):
-    return HttpResponse("Ваша запись была изменена успешно.")
-
-def task31_3(request):
-    return HttpResponse("Ваша запись была удалена.")
